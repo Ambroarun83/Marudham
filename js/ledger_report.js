dcf55fb7ecf99743084965ea7ba4131de1a50029
Ambroarun83 <54171691+Ambroarun83@users.noreply.github.com>
2023-10-06 18:58:06 +0530

    Ledger report
    daily ledger list fetched
    collection of the customer need to be fetched as per date

--- js/ledger_report.js
+++ js/ledger_report.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-    const toggleButtons = $(".toggle-button");
-    toggleButtons.on("click", function() {
+
+    $(".toggle-button").on("click", function() {
         // Reset active class for all buttons
-        toggleButtons.removeClass("active");
+        $(".toggle-button").removeClass("active");
         // Add active class to the clicked button
         $(this).addClass("active");
 
         let typevalue = this.value;//this will contain the selected value
+
+        $("#daily_card").toggle(typevalue === 'Daily');//this condition will throw true to toggle function which will show cards
+        $("#weekly_card").toggle(typevalue === 'Weekly');
+        $("#monthly_card").toggle(typevalue === 'Monthly');
+
+        if (typevalue === 'Daily') {getDailyTable();}
+        else if (typevalue === 'Weekly') {getWeeklyTable();}
+        else if (typevalue === 'Monthly') {getMonthlyTable();}
     });
-})
+
+});
+
+
+
+
+function getDailyTable(){
+    $.post('reportFile/ledger/getDailyTable.php',{},function(data){
+        $('#daily_table_div').empty().html(data);
+    });
+}
