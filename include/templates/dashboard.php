1b06714efa72de364251afae1888c5f5787c3e43
Ambroarun83 <arunnatarajan2704@gmail.com>
2024-03-05 17:47:50 +0530

    Dashboard
    Dashboard design for Request completed

--- include/templates/dashboard.php
+++ include/templates/dashboard.php
@@ -14,66 +14,19 @@ $getValues = $userObj->getDataForDashboard($mysqli, $userid);
 <link href="https://cdn.jsdelivr.net/npm/wowjs@1.1.3/css/libs/animate.min.css" rel="stylesheet">
 <!-- for counter -->
 <script src="https://unpkg.com/counterup2@2.0.2/dist/index.js"></script>
+<link rel="stylesheet" href="css/dashboard.css">
 
 <style>
-	/* .card{
-		background-color: #02c7b5;
-		color: white; 
-	} */
-	.heading-list {
-		font-size: 2rem;
-		font-weight: bold;
-		/* text-align: center; */
-	}
 
-	.counter-head {
-		font-size: 1.5rem;
-		font-weight: bold;
-	}
-
-	.counter {
-		font-size: 4rem;
-		font-family: fantasy;
-		font-weight: 100;
-	}
-
-	@media (max-width: 768px) {
-		/* .card {
-			height: 80vh;
-		} */
-
-		.counter {
-			font-size: 6.5rem;
-		}
-	}
-
-	@media (max-width: 600px) {
-		/* .card {
-			height: 77vh;
-		} */
-
-		.counter {
-			font-size: 4rem;
-		}
-	}
-
-	@media (max-width: 320px) {
-		/* .card {
-			height: 65vh;
-		} */
-
-		.counter {
-			font-size: 2.5rem;
-		}
-	}
 </style>
 <!-- Page header start -->
 <br><br>
-<div class="page-header">
+<br><br>
+<!-- <div class="page-header">
 	<div style="background-color:#009688; width:100%; padding:12px; color: #ffff; font-size: 20px; border-radius:5px;">
 		Marudham - Dashboard
 	</div>
-</div><br>
+</div><br> -->
 
 <!-- Page header end -->
 
@@ -237,13 +190,106 @@ $getValues = $userObj->getDataForDashboard($mysqli, $userid);
 				</div>
 			</div>
 		<?php } else { ?>
-			<div class="card wow">
+			<div class="card">
 				<div class="card-header">
-					<div class="card-title"></div>
+					<div class="card-title" id="req_title" style="display:flex;justify-content:center;align-items: center;font-size:1.5rem;cursor:pointer">Request</div>
 				</div>
-				<div class="card-body" style="display:flex;justify-content:center;align-items: center;">
-					<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-					<div id="chartContainer1" style="height: 370px; width: 100%;"></div>
+				<div class="card-body" id="req_body">
+					<div class="row" style="display:flex;justify-content:flex-start;">
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Total Requests</p>
+										<p class="counter wow fadeInUp" id="tot_req">100</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Total Issued</p>
+										<p class="counter wow fadeInUp" id="tot_issue">40</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Total Balance</p>
+										<p class="counter wow fadeInUp" id="tot_bal">50</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Today Requests</p>
+										<p class="counter wow fadeInUp" id="today_req">10</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Today Issued</p>
+										<p class="counter wow fadeInUp" id="today_issue">5</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-2">
+							<div class="card">
+								<div class="card-body counter-cards">
+									<div class="form-group text-center">
+										<p class='counter-head wow fadeIn'>Today Balance</p>
+										<p class="counter wow fadeInUp" id="today_bal">5</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-12">
+							<div class="card" style="border:3px solid #009688">
+								<div class="card-body">
+									<div class="radio-container">
+										<div class="selector">
+											<div class="selector-item">
+												<input type="radio" id="radio1" name="chart_selector" class="selector-item_radio" checked>
+												<label for="radio1" class="selector-item_label">Cancel & Revoke</label>
+											</div>
+											<div class="selector-item">
+												<input type="radio" id="radio2" name="chart_selector" class="selector-item_radio">
+												<label for="radio2" class="selector-item_label">Customer Type</label>
+											</div>
+											<div class="selector-item">
+												<input type="radio" id="radio3" name="chart_selector" class="selector-item_radio">
+												<label for="radio3" class="selector-item_label">Loan Category</label>
+											</div>
+										</div>
+									</div>
+									<br>
+									<div class="row" >
+										<!-- <div class="col-12"> -->
+										<div class="col-6" >
+											<div class="charts" id="chart1"></div>
+										</div>
+										<div class="col-6" >
+											<div class="charts" id="chart2"></div>
+										</div>
+										<!-- </div> -->
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		<?php } ?>
@@ -252,113 +298,5 @@ $getValues = $userObj->getDataForDashboard($mysqli, $userid);
 
 
 <script src="vendor/canvasjs/canvasjs.min.js"></script>
-<script>
-	document.addEventListener('DOMContentLoaded', () => {
-		new WOW().init();
-		const counterUp = window.counterUp.default;
-
-		const callback = entries => {
-			entries.forEach(entry => {
-				const el = entry.target;
-				if (entry.isIntersecting && !el.classList.contains('is-visible')) {
-					counterUp(el, {
-						// duration: 3000,
-						// delay: 20,
-					});
-					el.classList.add('is-visible');
-				}
-			});
-		};
-
-		const IO = new IntersectionObserver(callback, {
-			threshold: 0
-		});
-
-		const els = document.querySelectorAll('.counter');
-		els.forEach(el => {
-			IO.observe(el);
-		});
-
-		var chart = new CanvasJS.Chart("chartContainer", {
-			theme: "light1", // "light1", "light2", "dark1", "dark2"
-			// exportEnabled: true,
-			// animationEnabled: true,
-			title: {
-				text: "Desktop Browser Market Share in 2016"
-			},
-			data: [{
-				type: "pie",
-				startAngle: 25,
-				toolTipContent: "<b>{label}</b>: {y}",
-				showInLegend: "true",
-				legendText: "{label}",
-				indexLabelFontSize: 16,
-				indexLabel: "{label} - {y}",
-				dataPoints: [{
-					y: 51.08,
-					label: "Chrome"
-				}, {
-					y: 27.34,
-					label: "Internet Explorer"
-				}, {
-					y: 10.62,
-					label: "Firefox"
-				}, {
-					y: 5.02,
-					label: "Microsoft Edge"
-				}, {
-					y: 4.07,
-					label: "Safari"
-				}, {
-					y: 1.22,
-					label: "Opera"
-				}, {
-					y: 0.44,
-					label: "Others"
-				}],
-			}]
-		});
-		chart.render();
-		var chart1 = new CanvasJS.Chart("chartContainer1", {
-			theme: "light2", // "light1", "light2", "dark1", "dark2"
-			// exportEnabled: true,
-			// animationEnabled: true,
-			title: {
-				text: "Desktop Browser Market Share in 2016"
-			},
-			data: [{
-				type: "pie",
-				startAngle: 25,
-				toolTipContent: "<b>{label}</b>: {y}",
-				showInLegend: "true",
-				legendText: "{label}",
-				indexLabelFontSize: 16,
-				indexLabel: "{label} - {y}",
-				dataPoints: [{
-					y: 51.08,
-					label: "Chrome"
-				}, {
-					y: 27.34,
-					label: "Internet Explorer"
-				}, {
-					y: 10.62,
-					label: "Firefox"
-				}, {
-					y: 5.02,
-					label: "Microsoft Edge"
-				}, {
-					y: 4.07,
-					label: "Safari"
-				}, {
-					y: 1.22,
-					label: "Opera"
-				}, {
-					y: 0.44,
-					label: "Others"
-				}],
-			}]
-		});
-		chart1.render();
-		$('.canvasjs-chart-credit').hide();
-	});
-</script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src='js/dashboard.js'></script>
---
+++ js/dashboard.js
@@ -0,0 +1,261 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new WOW().init();
+    const counterUp = window.counterUp.default;
+
+    const callback = entries => {
+        entries.forEach(entry => {
+            const el = entry.target;
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    // duration: 3000,
+                    // delay: 20,
+                });
+                el.classList.add('is-visible');
+            }
+        });
+    };
+
+    const IO = new IntersectionObserver(callback, {
+        threshold: 0
+    });
+
+    const els = document.querySelectorAll('.counter');
+    els.forEach(el => {
+        IO.observe(el);
+    });
+
+    $('input[name="chart_selector"]').change(function () {
+        let selectedValue = $('input[name="chart_selector"]:checked').next().text().trim();
+        $('#chart1,#chart2').empty();
+    
+        if (selectedValue == 'Cancel & Revoke') {
+            tot_cr_chart();
+            tdy_cr_chart();
+        } else if (selectedValue == 'Customer Type') {
+            tot_ct_chart();
+            tdy_ct_chart();
+        } else if (selectedValue == 'Loan Category') {
+            tot_lc_chart();
+            tdy_lc_chart();
+        }
+    });
+    
+    $('input[name="chart_selector"]').trigger('change');//trigger at start
+    
+    
+    $('#req_title').click(function () {
+        let check = $('#req_body');
+        if (check.is(':visible')) {
+            check.slideUp();
+        } else {
+            check.slideDown();
+        }
+    });
+});
+
+
+//Cancel and Revoke Charts
+function tot_cr_chart() {
+    google.charts.load("current", { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["Request Status", "Count", { role: "style" }],
+            ["Cancel", 15, "#faae7b"],
+            ["Revoke", 5, "#432371"],
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+            {
+                calc: "stringify",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation"
+            },
+            2]);
+
+        var options = {
+            title: "Total Cancelled and Revoked",
+            width: '100%',
+            height: '400px',
+            bar: { groupWidth: "90%" },
+            legend: { position: "none" },
+            vAxis: { format: 'decimal', gridlines: { interval: [0, 5, 10, 15, 20] } },//this is for left vertical column count interval
+            chartArea: { 
+                
+            }
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("chart1"));
+        chart.draw(view, options);
+    }
+}
+function tdy_cr_chart() {
+    google.charts.load("current", { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["Request Status", "Count", { role: "style" }],
+            ["Cancel", 2, "#faae7b"],
+            ["Revoke", 3, "#432371"],
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+            {
+                calc: "stringify",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation"
+            },
+            2]);
+
+        var options = {
+            title: "Today Cancelled and Revoked",
+            width: '100%',
+            height: '400px',
+            bar: { groupWidth: "90%" },
+            legend: { position: "none" },
+            vAxis: { format: 'decimal', gridlines: { interval: [0, 5, 10, 15, 20] } },//this is for left vertical column count interval
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("chart2"));
+        chart.draw(view, options);
+    }
+}
+
+//Customer Type Charts
+function tot_ct_chart() {
+    google.charts.load("current", { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["Customer Type", "Count", { role: "style" }],
+            ["New Customer", 75, "#faae7b"],
+            ["Existing Customer", 25, "#432371"],
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+            {
+                calc: "stringify",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation"
+            },
+            2]);
+
+        var options = {
+            title: "Total New & Existing Customers",
+            width: '100%',
+            height: '400px',
+            bar: { groupWidth: "90%" },
+            legend: { position: "none" },
+            vAxis: { format: 'decimal', gridlines: { interval: [0, 5, 10, 15, 20] } },//this is for left vertical column count interval
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("chart1"));
+        chart.draw(view, options);
+    }
+}
+function tdy_ct_chart() {
+    google.charts.load("current", { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["Customer Type", "Count", { role: "style" }],
+            ["New Customer", 7, "#faae7b"],
+            ["Existing Customers", 2, "#432371"],
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+            {
+                calc: "stringify",
+                sourceColumn: 1,
+                type: "string",
+                role: "annotation"
+            },
+            2]);
+
+        var options = {
+            title: "Today New & Existing Customers",
+            width: '100%',
+            height: '400px',
+            bar: { groupWidth: "90%" },
+            legend: { position: "none" },
+            vAxis: { format: 'decimal', gridlines: { interval: [0, 5, 10, 15, 20] } },//this is for left vertical column count interval
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("chart2"));
+        chart.draw(view, options);
+    }
+}
+
+//Loan Category Charts
+function tot_lc_chart() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            ['Loan Category', 'Count'],
+            ['Appliance', 30],
+            ['Personal', 10],
+            ['Interest', 2],
+            ['Property', 5],
+            ['Gold', 15],
+            ['Education', 8],
+            ['Business', 20],
+            ['Home', 1]
+        ]);
+
+        var options = {
+            // is3D: true,
+            pieSliceText: 'label',
+            legend: { cursor: 'pointer'},
+            colors: ['#734f5a', '#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#941c2f', '#c05761'],
+            tooltip: { trigger: 'selection' },
+            width: '100%',
+            height: '100%',
+            title: 'Loan Category',
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart1'));
+        chart.draw(data, options);
+
+    }
+}
+function tdy_lc_chart() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            ['Loan Category', 'Count'],
+            ['Appliance', 51],
+            ['Personal', 27],
+            ['Interest', 10],
+            ['Property', 5],
+            ['Gold', 4],
+            ['Education', 1],
+            ['Business', 4],
+            ['Home', 15]
+        ]);
+
+        var options = {
+            // is3D: true,
+            pieSliceText: 'label',
+            legend: { cursor: 'pointer'},
+            colors: ['#734f5a', '#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#941c2f', '#c05761'],
+            tooltip: { trigger: 'selection' },
+            width: '100%',
+            height: '100%',
+            title: 'Loan Category',
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart2'));
+        chart.draw(data, options);
+
+    }
+    
+}
